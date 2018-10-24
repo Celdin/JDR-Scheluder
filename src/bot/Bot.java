4f77f34e5807a2c9b7289d802799b64c71f8aa7b
Celdin <Sylvain.goulliart@gmail.com>
2018-10-24 22:54:25 +0200

    new JDA

--- src/bot/Bot.java
+++ src/bot/Bot.java
@@ -18,11 +18,12 @@ public class Bot {
 	private JDA jda;
 
 	public Bot(String token) throws LoginException, InterruptedException {
-		jda = new JDABuilder(AccountType.BOT).setToken(token).setBulkDeleteSplittingEnabled(false).buildBlocking();
+		jda = new JDABuilder(AccountType.BOT).setToken(token).setBulkDeleteSplittingEnabled(false).build();
+		jda.awaitReady();
 		jda.getPresence().setGame(Game.of(Game.GameType.DEFAULT, "planifier"));
 		System.out.println("Connecte avec: " + jda.getSelfUser().getName());
 		int size = jda.getGuilds().size();
-		System.out.println("Autoris� sur " + size + " serveur" + (size > 1 ? "s" : ""));
+		System.out.println("Autorisé sur " + size + " serveur" + (size > 1 ? "s" : ""));
 		for (Guild guild : jda.getGuilds()) {
 			System.out.println("	 - " + guild.getName());
 		}
