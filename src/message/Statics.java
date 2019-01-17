85de5ca14c0338357cbf7db0dbed99d14a7e67e1
Sylvain Goulliart <Sylvain.goulliart@gmail.com>
2019-01-17 14:26:17 +0100

    Update Statics.java

--- src/message/Statics.java
+++ src/message/Statics.java
@@ -3,7 +3,7 @@ package message;
 public class Statics {
 	public final static String START_CMD = "$";
 
-	public static final String NON = "✖";
-	public static final String OUI = "✔";
+	public static final String NON = "❎";
+	public static final String OUI = "✅";
 
 }
