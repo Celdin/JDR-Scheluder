c7a248dee90686cd472762ee370ea8c1685a04b7
goulliarts <goulliarts@FR-L2986517.groupinfra.com>
2019-02-22 17:32:06 +0100

    fixs

--- src/scheduler/EventRunnable.java
+++ src/scheduler/EventRunnable.java
@@ -11,6 +11,7 @@ import data.query.EventQuery;
 import lombok.AllArgsConstructor;
 import message.Statics;
 import net.dv8tion.jda.core.entities.Message;
+import net.dv8tion.jda.core.entities.MessageReaction;
 import net.dv8tion.jda.core.entities.TextChannel;
 import net.dv8tion.jda.core.entities.User;
 
@@ -24,20 +25,27 @@ public class EventRunnable implements Runnable {
 		EventQuery eventQuery = new EventQuery();
 		CookerQuery cookerQuery = new CookerQuery();
 		Message message = channel.getMessageById(event.getAnnonceCooker().getId()).complete();
-		Optional<User> oui = message.getReactions().stream().filter(reaction -> Statics.OUI.equals(reaction.getReactionEmote().getName())).findAny().get().getUsers().stream().filter(user -> !channel.getJDA().getSelfUser().equals(user)).findFirst();
+		Optional<MessageReaction> hasOui = message.getReactions().stream()
+				.filter(reaction -> Statics.OUI.equals(reaction.getReactionEmote().getName())).findAny();
 		try {
-			if(oui.isPresent()) {
-				if(event.getHaveCooked().containsKey(oui.get())){
-					event.getHaveCooked().put(oui.get(), event.getHaveCooked().get(oui.get()) + 1);
-				} else {
-					OptionalDouble optionalAverage =event.getHaveCooked().values().stream().mapToInt(Integer::intValue).average();
-					if(optionalAverage.isPresent()) {
-						event.getHaveCooked().put(oui.get(), (int) optionalAverage.getAsDouble());
+			if (hasOui.isPresent()) {
+				Optional<User> oui = hasOui.get().getUsers().stream()
+						.filter(user -> !channel.getJDA().getSelfUser().equals(user)).findFirst();
+
+				if (oui.isPresent()) {
+					if (event.getHaveCooked().containsKey(oui.get())) {
+						event.getHaveCooked().put(oui.get(), event.getHaveCooked().get(oui.get()) + 1);
 					} else {
-						event.getHaveCooked().put(oui.get(), 1);
+						OptionalDouble optionalAverage = event.getHaveCooked().values().stream()
+								.mapToInt(Integer::intValue).average();
+						if (optionalAverage.isPresent()) {
+							event.getHaveCooked().put(oui.get(), (int) optionalAverage.getAsDouble());
+						} else {
+							event.getHaveCooked().put(oui.get(), 1);
+						}
 					}
+					cookerQuery.save(eventQuery.getId(event), event);
 				}
-				cookerQuery.save(eventQuery.getId(event), event);
 			}
 			event.setNextDate(EventScheduler.getNextSchedul(event).getTimeInMillis());
 			MessageManager.createMessages(channel, event);
